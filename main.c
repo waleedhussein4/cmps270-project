c5160bba8f0a279c181263e40c362c204b7754d8
Waleed <walid.a.hussein@hotmail.com>
2023-09-21 14:54:14 +0300

    create read function char** read(char* fileName) takes file name and returns array of strings containing each line

--- main.c
+++ main.c
@@ -1,5 +1,31 @@
 #include <stdio.h>
 
-int main () {
+#define MAX_LINES 100
+#define MAX_LEN 50
+
+char** read(char* fileName) {
+  FILE *file;
+  file = fopen(fileName, "r");
   
+  char** text = (char**)malloc(sizeof(char*) * MAX_LINES);
+  for (int i = 0; i < MAX_LINES; i++) {
+    text[i] = (char*)malloc(sizeof(char) * MAX_LEN);
+  }
+
+  int line = 0;
+  while ( !feof(file) && !ferror(file) ) { // keep reading until the end of the file
+    if (fgets(text[line], 50, file) != NULL) {
+      line++;
+    }
+  }
+
+  fclose(file);
+
+  return text;
+}
+
+int main () {
+  char** spells = read("spells.txt");
+
+  return 0;
 }
